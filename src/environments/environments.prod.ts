269504ed816256c2c5b04730bc53845def5005b3
mbuckles <bucklesm21@gmail.com>
2018-05-01 10:12:16 -0400

    added environment

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+    firebaseConfig: {
+         apiKey: "AIzaSyDjyvgyqkFI7CupDdU5arXtqiME2zdMSTo",
+         authDomain: "firestore-abae4.firebaseapp.com",
+         databaseURL: "https://firestore-abae4.firebaseio.com",
+         projectId: "firestore-abae4",
+         storageBucket: "firestore-abae4.appspot.com",
+         messagingSenderId: "969868001262"
+    }
+  };
